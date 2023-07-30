20b53678ae138ebf9c35f0cf0d6e680a300726d2
Noah Poling <noahspoling@gmail.com>
2023-07-30 16:03:38 -0400

    feature: added files for user management forms as well as worked on the user mvc model. flask_login has a much better support for users so I used that instead of writing my own service. authCheck.py handles methods for my custom fields.

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ This the the repository for my personal blog to eventually publish my various pa
 
   For learning a new static file web server that can make use of python's vast module library for data science in the future
 
+This project supports a user system for multiple authors and comments. The contact me form can be used in the eventual implementation of the admin dashboard to control the role users have. 
+
+TODO:
+* Implement a series
+
+Will require a custom search select option input for large number of post series.
+
--- app/users/controller.py
+++ app/users/controller.py
@@ -1,3 +1,7 @@
+from flask import Flask, Blueprint, render_template, request, redirect, url_for, flash
+from flask_login import current_user, login_user, logout_user, login_required
+from app.users.form import LoginForm, RegistrationForm
+from app.users.model import User
 from flask_login import LoginManager
 
 login = LoginManager()
@@ -8,4 +12,22 @@ def load_user(id):
 
 userBlueprint = Blueprint('posts', __name__, url_prefix="/user")
 
-@userBlueprint('/login', methods=[])
+@userBlueprint('/login', methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("index"))
+    form = LoginForm()
+    if form.valiate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@userBlueprint.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
